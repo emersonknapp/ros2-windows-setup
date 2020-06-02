1240dbc137391a416ef7dfca3e0c8b8981a85fbc
Emerson Knapp <eknapp@amazon.com>
2020-06-01 19:33:48 -0700

    WIP install visual studio and ros2 dependencies
    Signed-off-by: Emerson Knapp <eknapp@amazon.com>

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ Open Powershell terminal
 START https://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/emersonknapp/ros2-windows-setup/master/boxstarter/ros2-core.ps1
 
 ```
+
+The first time you run, it will prompt you to add boxstarter.org to the trusted list. Press "add" a few times, then re-run the powershell command
+
+
+```
+.\vs_community__1990310475.1591058169.exe --passive `
+  --add Microsoft.VisualStudio.Product.Community `
+  --add Microsoft.VisualStudio.Component.VC.CoreIde `
+  --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64
+```
--- boxstarter/ros2-core.ps1
+++ boxstarter/ros2-core.ps1
@@ -8,3 +8,21 @@ cinst vcredist2013
 cinst vcredist140
 
 cinst visualstudio2019community -package-parameters "--config $((Get-Location).Path + '\vsconfig')"
+
+cinst cmake
+
+New-Item -ItemType Directory -Force -Path \dev\ros2\tmp
+curl https://github.com/ros2/choco-packages/releases/download/2020-02-24/asio.1.12.1.nupkg -o \dev\ros2\tmp\asio.1.12.1.nupkg
+curl https://github.com/ros2/choco-packages/releases/download/2020-02-24/cunit.2.1.3.nupkg -o \dev\ros2\tmp\cunit.2.1.3.nupkg
+curl https://github.com/ros2/choco-packages/releases/download/2020-02-24/eigen.3.3.4.nupkg -o \dev\ros2\tmp\eigen.3.3.4.nupkg
+curl https://github.com/ros2/choco-packages/releases/download/2020-02-24/log4cxx.0.10.0.nupkg -o \dev\ros2\tmp\log4cxx.0.10.0.nupkg
+curl https://github.com/ros2/choco-packages/releases/download/2020-02-24/tinyxml-usestl.2.6.2.nupkg -o \dev\ros2\tmp\tinyxml-usestl.2.6.2.nupkg
+curl https://github.com/ros2/choco-packages/releases/download/2020-02-24/tinyxml2.6.0.0.nupkg -o \dev\ros2\tmp\tinyxml2.6.0.0.nupkg
+
+cinst -s \dev\ros2\tmp `
+  asio `
+  cunit `
+  eigen `
+  log4cxx `
+  tinyxml-usestl `
+  tinyxml2
